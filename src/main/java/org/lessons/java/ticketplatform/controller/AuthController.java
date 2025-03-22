76bee5959de90e4bb44ccc9aa2e29502dec89d7e
davideblaffard <infodavide98@gmail.com>
2025-03-22 13:06:08 +0100

    add: created authorization controller

---
+++ src/main/java/org/lessons/java/ticketplatform/controller/AuthController.java
@@ -0,0 +1,31 @@
+package org.lessons.java.ticketplatform.controller;
+
+import org.lessons.java.ticketplatform.model.enums.Role;
+import org.lessons.java.ticketplatform.model.Operator;
+import org.lessons.java.ticketplatform.repository.OperatorRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+    
+    @Autowired
+    private OperatorRepository operatorRepository;
+
+    @GetMapping("/login")
+    public String loginPage(){
+        return "login";
+    }
+
+    @GetMapping("/redirect")
+    public String redirectAfterLogin(@AuthenticationPrincipal Operator loggedOperator){
+        if(loggedOperator.getRole() == Role.ADMIN){
+            return "redirect:/admin/dashboard";
+        } else {
+            return "redirect:/operator/dashboard";
+        }
+        }
+}
