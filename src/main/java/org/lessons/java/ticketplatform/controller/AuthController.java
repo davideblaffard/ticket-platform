c9bdb2ad85e67288ddb54dd53aee54a26e32f3ab
davideblaffard <infodavide98@gmail.com>
2025-03-25 16:32:24 +0100

    feat: cleaned code

--- src/main/java/org/lessons/java/ticketplatform/controller/AuthController.java
+++ src/main/java/org/lessons/java/ticketplatform/controller/AuthController.java
@@ -1,10 +1,11 @@
 package org.lessons.java.ticketplatform.controller;
 
 import org.lessons.java.ticketplatform.model.enums.Role;
+import org.lessons.java.ticketplatform.security.DatabaseOperatorDetails;
 import org.lessons.java.ticketplatform.model.Operator;
-import org.lessons.java.ticketplatform.repository.OperatorRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.lessons.java.ticketplatform.repository.OperatorRepository;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AuthController {
     
-    @Autowired
-    private OperatorRepository operatorRepository;
+    // @Autowired
+    // private OperatorRepository operatorRepository;
 
     @GetMapping("/login")
     public String loginPage(){
@@ -21,15 +22,11 @@ public class AuthController {
     }
 
     @GetMapping("/redirect")
-    public String redirectAfterLogin(@AuthenticationPrincipal Operator loggedOperator){
-        if (loggedOperator == null) {
-            return "redirect:/login?error";
-        }
-    
-        if (loggedOperator.getRole() == Role.ADMIN) {
-            return "redirect:/admin/dashboard";
+    public String redirectAfterLogin(@AuthenticationPrincipal DatabaseOperatorDetails loggedUser) {
+        if (loggedUser.getRole() == Role.ADMIN) {
+            return "redirect:/admin/dashboard";     // AdminController
         } else {
-            return "redirect:/operator/dashboard";
+            return "redirect:/operator/dashboard"; // OperatorController
         }
-        }
+    }
 }
